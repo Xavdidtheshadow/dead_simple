d628dcd6ba13e61916013115b68268208d0c3122
David Brownman <beamneocube@gmail.com>
2016-02-22 00:59:51 -0800

    jump to bottom, messages, styling, heroku readiness

--- app/client.js
+++ app/client.js
@@ -1,18 +1,3 @@
-// simple client
-
-// var socket = io();
-// var id = 0;
-
-// $('form').submit(function(){
-//   id += 1;
-//   socket.emit('message', {message: $('#m').val(), room: '#{room}'});
-//   $('#m').val('');
-//   return false;
-// });
-
-// socket.on('message:#{room}', function(msg){
-//   $('.messages').append($('<li>').text(msg.message));
-// });
 
 var angular = require('angular');
 var socket = require('socket.io-client')();
@@ -21,17 +6,32 @@ var utils = require('./utils');
 var glue = require('angularjs-scroll-glue');
 
 var app = angular.module('simpleApp', ['luegg.directives'])
-  .controller('RoomController', ['$scope', '$timeout', function($scope, $timeout) {
+  .controller('RoomController', ['$scope', '$timeout', '$anchorScroll', function($scope, $timeout, $anchorScroll) {
 
     $scope.init = function(room) {
-      $scope.room = room;
+      $scope.room = room; // passed in from controller
       $scope.messages = [];
-      // $scope.user = colors.color_pair();
+      $scope.model = {
+        text: '' // init to string so we can submit empty box
+      };
+      
       $scope.user = {id: utils.randomId()};
-      // we always start with just us? 
-      $scope.user_count = 1;
-      // console.log($scope.user);
-      // console.log(utils.randomId());
+      
+      // $scope.user_count = 1;
+      $scope.glued = true;
+      $scope.glued_to = 0;
+
+      $scope.message_count = 0;
+      
+      $scope.$watch('glued', function(newVal, oldVal) {
+        if (newVal === oldVal) { return; }
+
+        if (newVal === false) {
+          // store the number for the last message they saw
+          $scope.glued_to = $scope.message_count;
+        }
+        // console.log(oldVal,'is now',newVal);
+      });
 
       // socket.on('connect', function() {
       //   console.log('connected');
@@ -48,24 +48,28 @@ var app = angular.module('simpleApp', ['luegg.directives'])
         // timeout cause https://stackoverflow.com/questions/30976934/socket-io-message-doesnt-update-angular-variable
         var color;
         if (msg.user.id === $scope.user.id) {
-          color = '#22bbff';
+          color = 'rgba(116, 187, 255, 0.6)';
         } else {
-          color = '#cdcdcd';
+          color = 'rgba(205, 205, 205, 0.6)';
         }
 
         msg.style = {'background-color': color};
         $timeout(function() {
+          $scope.message_count += 1;
           $scope.messages.push(msg);
         });
       });
 
-      socket.on('users:' + $scope.room, function(data){
-        console.log(data);
-        $timeout(function() {
-          $scope.user_count = data.count;
-        });
-      });
+      // socket.on('users:' + $scope.room, function(data){
+      //   console.log(data);
+      //   $timeout(function() {
+      //     $scope.user_count = data.count;
+      //   });
+      // });
+    };
 
+    $scope.scroll_to_bottom = function() {
+      $anchorScroll('bottom');
     };
 
     $scope.submit = function() {
